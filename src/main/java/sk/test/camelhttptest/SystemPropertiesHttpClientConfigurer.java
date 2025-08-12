a85f9e18ff7e9b6b0d6c38fcd535374bf0ea2c84
agiertli <agiertli@redhat.com>
2025-08-12 15:10:35 +0200

    improved code

--- src/main/java/sk/test/camelhttptest/SystemPropertiesHttpClientConfigurer.java
+++ src/main/java/sk/test/camelhttptest/SystemPropertiesHttpClientConfigurer.java
@@ -6,14 +6,33 @@ import org.apache.hc.client5.http.impl.routing.DefaultRoutePlanner;
 import org.apache.hc.core5.http.HttpException;
 import org.apache.hc.core5.http.HttpHost;
 import org.apache.hc.core5.http.protocol.HttpContext;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import io.quarkus.logging.Log;
+import jakarta.inject.Inject;
+import org.apache.camel.CamelContext;
 
 @jakarta.enterprise.context.ApplicationScoped
 public class SystemPropertiesHttpClientConfigurer implements HttpClientConfigurer {
 
+    @Inject
+    CamelContext camelContext;
+
+    @ConfigProperty(name = "http.proxyHost")
+    String property_proxyHost;
+
+    @ConfigProperty(name = "http.proxyPort")
+    Integer property_proxyPort;
+
+    @ConfigProperty(name = "http.proxyEnabled")
+    Boolean enableProxy;
+
     @Override
     public void configureHttpClient(HttpClientBuilder clientBuilder) {
-        String proxyHost = System.getProperty("http.proxyHost");
-        String proxyPort = System.getProperty("http.proxyPort");
+        String proxyHost = property_proxyHost; // alternative approach: System.getProperty("http.proxyHost")
+        String proxyPort = String.valueOf(property_proxyPort);  // alternative approach: System.getProperty("http.proxyPort")
+
+        Log.info("Setting proxy host and port on http-proxy component: " + proxyHost + ":" + proxyPort);
 
         if (proxyHost != null && proxyPort != null) {
             HttpHost proxy = new HttpHost(proxyHost, Integer.parseInt(proxyPort));
